fc131679cd95e00620f50c3757d2e770734c5cdf
Walter Anderson <walter.anderson@harborhealthteam.com>
2024-12-10 00:33:30 -0600

    day 6 part 2

--- day-6/day6.go
+++ day-6/day6.go
@@ -1,11 +1,118 @@
 package day6
 
 import (
-	"bufio"
+	"aoc/utils"
+	"bytes"
+	"errors"
 	"fmt"
 	"log"
-	"os"
+	"strings"
 )
+
+func Run(filename string) {
+	guardMap := make(GuardMap)
+	startingPos := Position{}
+	y := 0
+	utils.ReadLines(filename, func(line string) {
+		for x, ch := range line {
+			guardMap[Position{x: x, y: y}] = ch
+			if ch == '^' {
+				startingPos.x = x
+				startingPos.y = y
+			}
+		}
+		y++
+	})
+	sum := utils.NewSet[Position]()
+	sum.Add(startingPos)
+	patrol := NewPatrol(startingPos, guardMap, North)
+	patrol.Start(func(pos Position) {
+		sum.Add(pos)
+	})
+	fmt.Printf("Part One: %d\n\n", sum.Size())
+
+	sim := NewSimulator(patrol)
+	sim.Simulate()
+	fmt.Println(sim)
+}
+
+var LoopError = errors.New("LOOP")
+
+type MapSimulation struct {
+	guardMap GuardMap
+	obstacle Position
+}
+
+type Simulator struct {
+	origin    *Patrol
+	maps      []MapSimulation
+	loopCount int
+}
+
+func NewSimulator(origin *Patrol) *Simulator {
+	return &Simulator{
+		origin:    origin,
+		maps:      nil,
+		loopCount: 0,
+	}
+}
+
+func (p *Simulator) String() string {
+	var out bytes.Buffer
+	out.WriteString("===SIMULATIONS===\n")
+	out.WriteString(fmt.Sprintf("maps=%d\n", len(p.maps)))
+	out.WriteString(fmt.Sprintf("Loop Count=%d\n", p.loopCount))
+	return out.String()
+}
+
+func (s *Simulator) Simulate() {
+	log.Println("Starting simulation")
+	s.getTestMaps()
+	log.Printf("Simulating %d Maps\n", len(s.maps))
+
+	mset := utils.NewSet[Position]()
+	for _, m := range s.maps {
+		if mset.Contains(m.obstacle) {
+			continue
+		}
+		mset.Add(m.obstacle)
+		patrol := NewPatrol(s.origin.startingPos, m.guardMap, s.origin.startingDir)
+		s.runPatrol(patrol, m.obstacle)
+	}
+}
+
+func (s *Simulator) runPatrol(patrol *Patrol, obstacle Position) {
+	err := patrol.Start(func(p Position) {})
+	if errors.Is(err, LoopError) {
+		fmt.Printf("Loop!: x=%d,y=%v\n", obstacle.x, obstacle.y)
+		fmt.Println(patrol.Draw())
+		s.loopCount++
+	}
+}
+
+func (s *Simulator) getTestMaps() {
+	for _, h := range s.origin.history {
+		totest := []Position{
+			{x: h.pos.x, y: h.pos.y - 1},
+			{x: h.pos.x + 1, y: h.pos.y},
+			{x: h.pos.x, y: h.pos.y + 1},
+			{x: h.pos.x - 1, y: h.pos.y},
+		}
+		for _, pos := range totest {
+			if s.origin.guardMap.Get(&pos) == '#' {
+				continue
+			}
+			guardMap := make(GuardMap)
+			for k, v := range s.origin.guardMap {
+				guardMap[k] = v
+			}
+			guardMap[pos] = '0'
+
+			m := MapSimulation{guardMap: guardMap, obstacle: pos}
+			s.maps = append(s.maps, m)
+		}
+	}
+}
 
 type Direction int
 
@@ -20,14 +127,14 @@ func (d Direction) String() string {
 	return [...]string{"North", "East", "South", "West"}[d]
 }
 
-type Point struct {
+type Position struct {
 	x int
 	y int
 }
 
-type GuardMap map[Point]rune
+type GuardMap map[Position]rune
 
-func (g GuardMap) Get(p *Point) rune {
+func (g GuardMap) Get(p *Position) rune {
 	ch, ok := g[*p]
 	if !ok {
 		return 0
@@ -35,32 +142,61 @@ func (g GuardMap) Get(p *Point) rune {
 	return ch
 }
 
-type PointSet map[Point]struct{}
-
-func (ps PointSet) Add(p Point) {
-	if !ps.Contains(p) {
-		ps[p] = struct{}{}
-	}
-}
-
-func (ps PointSet) Contains(p Point) bool {
-	_, exists := ps[p]
-	return exists
-}
-
-func (ps PointSet) Size() int {
-	return len(ps)
+type PatrolHistory struct {
+	pos          Position
+	dir          Direction
+	intersecting bool
 }
 
 type Patrol struct {
-	pos      Point
-	dir      Direction
-	guardMap GuardMap
-	partOne  PointSet
+	pos         Position
+	dir         Direction
+	history     []PatrolHistory
+	hSet        utils.Set[PatrolHistory]
+	guardMap    GuardMap
+	startingPos Position
+	startingDir Direction
+}
+
+func NewPatrol(pos Position, guardMap GuardMap, dir Direction) *Patrol {
+	return &Patrol{
+		pos:         pos,
+		dir:         dir,
+		history:     nil,
+		hSet:        utils.NewSet[PatrolHistory](),
+		guardMap:    guardMap,
+		startingPos: pos,
+		startingDir: dir,
+	}
+}
+
+func (p *Patrol) Start(onMove func(p Position)) error {
+	for p.isWalking() {
+		if p.isLoop() {
+			return LoopError
+		}
+		switch p.Peek() {
+		case '.', '^':
+			p.Move()
+			onMove(p.pos)
+		case '#', '0':
+			p.Turn()
+		}
+	}
+	return nil
+}
+
+func (p *Patrol) isLoop() bool {
+	ht := PatrolHistory{pos: p.pos, dir: p.dir, intersecting: true}
+	hf := PatrolHistory{pos: p.pos, dir: p.dir, intersecting: false}
+	if p.hSet.Contains(ht) || p.hSet.Contains(hf) {
+		return true
+	}
+	return false
 }
 
 func (p *Patrol) Peek() rune {
-	peek := Point{}
+	peek := Position{}
 	peek.x = p.pos.x
 	peek.y = p.pos.y
 	switch p.dir {
@@ -81,6 +217,7 @@ func (p *Patrol) isWalking() bool {
 }
 
 func (p *Patrol) Move() {
+	hist := PatrolHistory{pos: p.pos, dir: p.dir, intersecting: false}
 	switch p.dir {
 	case North:
 		p.pos.y--
@@ -91,7 +228,13 @@ func (p *Patrol) Move() {
 	case West:
 		p.pos.x--
 	}
-	p.partOne.Add(p.pos)
+	for _, h := range p.history {
+		if hist.pos == h.pos {
+			hist.intersecting = true
+		}
+	}
+	p.history = append(p.history, hist)
+	p.hSet.Add(hist)
 }
 
 func (p *Patrol) Turn() {
@@ -105,52 +248,72 @@ func (p *Patrol) Turn() {
 	case West:
 		p.dir = North
 	}
+	p.history = append(p.history, PatrolHistory{pos: p.pos, dir: p.dir, intersecting: false})
 }
 
-func NewPatrol(filename string) *Patrol {
-	file, err := os.Open(filename)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
-	sc := bufio.NewScanner(file)
-	guardMap := make(GuardMap)
-	startingPos := Point{}
-	y := 0
-	for sc.Scan() {
-		for x, ch := range sc.Text() {
-			guardMap[Point{x: x, y: y}] = ch
-			if ch == '^' {
-				startingPos.x = x
-				startingPos.y = y
-			}
+func (p *Patrol) Draw() string {
+	maxX, maxY := 0, 0
+	for pos := range p.guardMap {
+		if pos.x > maxX {
+			maxX = pos.x
 		}
-		y++
+		if pos.y > maxY {
+			maxY = pos.y
+		}
 	}
-	partOne := PointSet{}
-	partOne.Add(startingPos)
-	return &Patrol{
-		pos:      startingPos,
-		dir:      North,
-		guardMap: guardMap,
-		partOne:  partOne,
+	grid := make([][]rune, maxY+1)
+	for i := range grid {
+		grid[i] = make([]rune, maxX+1)
 	}
+	for pos, ch := range p.guardMap {
+		grid[pos.y][pos.x] = ch
+	}
+	for _, h := range p.history {
+		var ch rune
+		switch h.dir {
+		case North, South:
+			ch = '|'
+		case East, West:
+			ch = '-'
+		}
+		if h.intersecting {
+			ch = '+'
+		}
+		grid[h.pos.y][h.pos.x] = ch
+	}
+
+	grid[p.startingPos.y][p.startingPos.x] = '*'
+
+	var ch rune
+	switch p.dir {
+	case North:
+		ch = '^'
+	case South:
+		ch = 'v'
+	case East:
+		ch = '>'
+	case West:
+		ch = '<'
+	}
+	grid[p.pos.y][p.pos.x] = ch
+
+	var sb strings.Builder
+	for _, row := range grid {
+		sb.WriteString(string(row))
+		sb.WriteString("\n")
+	}
+	return sb.String()
 }
 
 func (p *Patrol) String() string {
-	return fmt.Sprintf("Position: %+v, Direction: %+v", p.pos, p.dir)
-}
-
-func Run(filename string) {
-	patrol := NewPatrol(filename)
-	for patrol.isWalking() {
-		switch patrol.Peek() {
-		case '.', '^':
-			patrol.Move()
-		case '#':
-			patrol.Turn()
-			patrol.Move()
-		}
+	var out bytes.Buffer
+	out.WriteString("===PATROL===\n")
+	out.WriteString("Map: ")
+	if len(p.guardMap) > 1000 {
+		out.WriteString("REDACTED\n")
+	} else {
+		out.WriteString("\n")
+		out.WriteString(p.Draw())
 	}
-	log.Printf("Part One: %d\n", patrol.partOne.Size())
+	return out.String()
 }
